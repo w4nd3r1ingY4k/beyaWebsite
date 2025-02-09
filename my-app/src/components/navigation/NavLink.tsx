280ae8a6ea5595c04565c32411ae97f463222042
Austin Liu <austin.liu05@yahoo.com>
2025-02-09 16:05:00 -0500

    Changed navigation link font

--- my-app/src/components/navigation/NavLink.tsx
+++ my-app/src/components/navigation/NavLink.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavLinkProps } from "../../ComponentTypes";
 
 export const NavLink: React.FC<NavLinkProps> = ({ text, isActive = false }) => {
-  const baseStyles = "text-xl font-semibold text-gray-800 uppercase";
+  const baseStyles = "text-xl font-jost";
   const activeStyles = "text-indigo-600 underline decoration-auto decoration-solid underline-offset-auto";
   
   return (
